29c0dba034cada13794d5fd3c999a97edacdaaf4
YUKIF69 <159157261+YUKIF69@users.noreply.github.com>
2025-04-28 07:46:22 +0300

    thirt commit

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@ const getTotalBalanceByGender = (users, gender) => {
     .filter(user => user.gender === gender)
     .map(user => user.balance)
     .reduce((total, balance) => {
-      total + balance;
+      return total + balance;
     }, 0);
 };
 
