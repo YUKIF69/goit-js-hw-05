560693a5771a98a658c73abba4f1cfe41268a147
YUKIF69 <159157261+YUKIF69@users.noreply.github.com>
2025-04-29 19:04:36 +0300

    Упрощен код функции getTotalBalanceByGender, убраны лишние фигурные скобки и оператор return

--- js/task-4.js
+++ js/task-4.js
@@ -1,11 +1,8 @@
-const getTotalBalanceByGender = (users, gender) => {
+const getTotalBalanceByGender = (users, gender) =>
   users
     .filter(user => user.gender === gender)
     .map(user => user.balance)
-    .reduce((total, balance) => {
-      return total + balance;
-    }, 0);
-};
+    .reduce((total, balance) => total + balance, 0);
 
 const clients = [
   {
